4f2ffe8c20e7c46af318ce64e9bca1aa9e7650f0
github username <github email address>
2019-12-01 10:26:40 -0600

    added generate whole numbers

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ function randomFraction() {
   
     // Only change code above this line.
   }
+
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
